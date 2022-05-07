6442ac2d6b907916e3f0e6f52e68f6b54666c9dd
wwwma <www.hosamawad2016@gmail.com>
2022-05-08 02:59:20 +0300

    finish app

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/screens/registration_screen.dart';
+import 'package:chat_app/screens/signin_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/welcome_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MessageMe app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: ChatScreen(),
+      initialRoute:
+          _auth != null ? ChatScreen.screenRoute : WelcomeScreen.screenRoute,
+      routes: {
+        WelcomeScreen.screenRoute: (context) => const WelcomeScreen(),
+        SignInScreen.screenRoute: (context) => const SignInScreen(),
+        RegistrationScreen.screenRoute: (context) => const RegistrationScreen(),
+        ChatScreen.screenRoute: (context) => const ChatScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,237 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+final _fireStore = FirebaseFirestore.instance;
+
+///This will give us the email
+late User signedInUser;
+
+class ChatScreen extends StatefulWidget {
+  static const String screenRoute = 'chat_screen';
+
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final messageTextController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
+
+  ///This will give us the message
+  String? messageText;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    try {
+      final user = _auth.currentUser;
+      if (user != null) {
+        signedInUser = user;
+        print(signedInUser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // void getMessages() async {
+  //   final messages = await _fireStore.collection('messages').get();
+  //   for (var message in messages.docs) {
+  //     print(message.data());
+  //   }
+  // }
+
+  // void messagesStreams() async {
+  //   await for (var snapshot in _fireStore.collection('messages').snapshots()) {
+  //     for (var message in snapshot.docs) {
+  //       print(message.data());
+  //     }
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.yellow[900],
+        title: Row(
+          children: [
+            Image.asset('images/logo.png', height: 25),
+            const SizedBox(width: 10),
+            const Text('MessageMe')
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _auth.signOut();
+              Navigator.pop(context);
+              // getMessages();
+              // messagesStreams();
+            },
+            icon: const Icon(Icons.close),
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const MessageStreamBuilder(),
+            Container(
+              decoration: const BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: Colors.orange,
+                    width: 2,
+                  ),
+                ),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: messageTextController,
+                      onChanged: (value) {
+                        messageText = value;
+                      },
+                      decoration: const InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 20,
+                        ),
+                        hintText: 'Write your message here...',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      _fireStore.collection('messages').add({
+                        'text': messageText,
+                        'sender': signedInUser.email,
+                        'time': FieldValue.serverTimestamp(),
+                      });
+                      messageTextController.clear();
+                    },
+                    child: Text(
+                      'send',
+                      style: TextStyle(
+                        color: Colors.blue[800],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MessageStreamBuilder extends StatelessWidget {
+  const MessageStreamBuilder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _fireStore.collection('messages').orderBy('time').snapshots(),
+      builder: (context, snapshot) {
+        List<MessageLine> messageWidgets = [];
+
+        if (!snapshot.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.blue,
+            ),
+          );
+        }
+
+        final messages = snapshot.data!.docs.reversed;
+        for (var message in messages) {
+          final messageText = message.get('text');
+          final messageSender = message.get('sender');
+          final currentUser = signedInUser.email;
+          final messageWidget = MessageLine(
+            sender: messageSender,
+            text: messageText,
+            isMe: currentUser == messageSender,
+          );
+          messageWidgets.add(messageWidget);
+        }
+        return Expanded(
+          child: ListView(
+            reverse: true,
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            children: messageWidgets,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MessageLine extends StatelessWidget {
+  final String? sender;
+  final String? text;
+  final bool isMe;
+
+  const MessageLine({this.text, this.sender, required this.isMe, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(
+            '$sender',
+            style: TextStyle(fontSize: 12, color: Colors.yellow[900]),
+          ),
+          Material(
+            elevation: 5,
+            color: isMe ? Colors.blue[800] : Colors.white,
+            borderRadius: isMe
+                ? const BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                    bottomLeft: Radius.circular(30),
+                  )
+                : const BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                    bottomLeft: Radius.circular(30),
+                  ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Text(
+                '$text',
+                style: TextStyle(
+                  fontSize: 13,
+                  color: isMe ? Colors.white : Colors.black45,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
