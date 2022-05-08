039e2536a881eba7da1a3b195e297325c8f1107d
wwwma <www.hosamawad2016@gmail.com>
2022-05-08 03:53:10 +0300

    simple update in all screens

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -25,6 +25,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+
       // appBar: AppBar(
       //   backgroundColor: Colors.white,
       //   elevation: 0,
